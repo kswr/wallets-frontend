2f9c99ede3cc302e4a12d2d6a5fd238084a84dd9
kswr <ksawery.oleszynski@gmail.com>
2018-12-13 12:52:24 +0100

    added landing page and stub login page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+// import Account from './views/Account.vue'
+import Login from './views/Login.vue'
+// import Main from './views/Main.vue'
+//
 
 Vue.use(Router)
 
@@ -14,12 +18,15 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
+
+// refactor to this:
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+// component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
---
+++ src/views/Main.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main">
+        <h1>This is main app page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
