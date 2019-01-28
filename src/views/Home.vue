e5abcf881259de9c3aabb444195e73c74388d7b4
kswr <ksawery.oleszynski@gmail.com>
2019-01-28 17:17:08 +0100

    modified landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
-    <div id="home" class="fill-centre">
-            <v-container class="fill-height">
+    <div id="home" >
+            <v-container class="fill-height" style="margin-left: 0">
                 <v-layout class="justify-center align-center">
-                    <v-flex xs10 sm7 md5 lg4 xl3 class="lighten-3" style=" border-radius: 20px">
+                    <v-flex xs12 sm10 md9 lg8 xl7 class="lighten-3" style=" border-radius: 20px">
                         <div>
-                            <!--<p class="display-1 white&#45;&#45;text text&#45;&#45;darken-3">WPS WALLETS</p>-->
-                            <!--<p class="white&#45;&#45;text">Simple app for real-life viewing, managing and controlling your interactive sales wallet</p>-->
-                            <!--<v-btn large color="light-green darken-1" class="white&#45;&#45;text" to="login">Sign in</v-btn>-->
-                            <!--<p class="white&#45;&#45;text">or</p>-->
-                            <!--<v-btn large color="light-green darken-1" class="white&#45;&#45;text" to="signup">Create an account</v-btn>-->
+                            <!--<p class="display-4 white&#45;&#45;text text&#45;&#45;darken-3 font-weight-bold shadow1" >WPS </p>-->
+                            <!--<p class="display-4 white&#45;&#45;text text&#45;&#45;darken-3 font-weight-bold shadow1">WALLETS</p>-->
+                            <p :class="fontSize">WPS</p>
+                            <p :class="fontSize">WALLETS</p>
+                            <p class="display-1 white--text shadow1">Simple app for real-life viewing, managing and controlling your interactive sales wallet</p>
+                            <v-btn large color="light-green darken-3" class="white--text elevation-5" to="signup">Create an account</v-btn>
+                            <v-btn large color="light-green darken-1" class="white--text elevation-5" to="login">Sign in</v-btn>
                         </div>
                     </v-flex>
                 </v-layout>
@@ -19,7 +21,15 @@
 <script>
   export default {
     components: {
-    }
+    },
+      computed: {
+        fontSize() {
+            switch(this.$vuetify.breakpoint.name) {
+                case 'xs': return 'display-3 white--text text--darken-3 font-weight-bold shadow1';
+                default: return 'display-4 white--text text--darken-3 font-weight-bold shadow1';
+            }
+        }
+      }
   }
 </script>
 
@@ -33,6 +43,10 @@
         -moz-background-size: cover;
         -o-background-size: cover;
         background-size: cover;
+    }
+
+    .shadow1 {
+        text-shadow: 2px 4px 3px rgba(0,0,0,0.3)
     }
 </style>
 
