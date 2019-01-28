c0cf3b12b6bc13ce7cf22f2c12e54906d2085549
kswr <ksawery.oleszynski@gmail.com>
2019-01-28 12:39:12 +0100

    added buttons to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="home">
-      <!--<v-img src="/google-earth-temp.jpg" class="home2"></v-img>-->
+    <div id="home" class="fill-centre">
+            <v-container class="fill-height">
+                <v-layout class="justify-center align-center">
+                    <v-flex xs10 sm7 md5 lg4 xl3 class="lighten-3" style=" border-radius: 20px">
+                        <div>
+                            <p class="display-1 white--text text--darken-3">WPS WALLETS</p>
+                            <p class="white--text">Simple app for real-life viewing, managing and controlling your interactive sales wallet</p>
+                            <v-btn large color="light-green darken-1" class="white--text" to="login">Sign in</v-btn>
+                            <p class="white--text">or</p>
+                            <v-btn large color="light-green darken-1" class="white--text" to="signup">Create an account</v-btn>
+                        </div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
     </div>
 </template>
 
@@ -12,10 +24,10 @@
 </script>
 
 <style scoped>
-    .home {
+    #home {
         height: 100%;
     }
-    .home {
+    #home {
         background: url(/google-earth-temp.jpg) no-repeat center center fixed;
         -webkit-background-size: cover;
         -moz-background-size: cover;
