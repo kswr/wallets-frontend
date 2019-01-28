52a141fa254a2fccf7ee22c5c0fa95b41c656c36
kswr <ksawery.oleszynski@gmail.com>
2019-01-28 01:29:53 +0100

    added retrieve password page

--- src/views/Registered.vue
+++ src/views/Registered.vue
@@ -1,16 +1,15 @@
 <template>
-    <div id="registered">
-        <v-form ref="loginForm" class="fill-height">
-            <v-container class="fill-height">
-                <v-layout class="justify-center align-center">
-                    <v-flex xs12 sm10 md9 lg8 xl7 class="grey lighten-3 elevation-10" style=" border-radius: 20px">
-                        <div style="padding: 20px 30px;">
-                            <p class="headline grey--text text--darken-3" style="margin: 0">User registered successfully, in <b>{{ time }}</b> you will be redirected to starting page</p>
-                        </div>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-form>
+    <div id="registered" ref="loginForm" class="fill-height">
+        <v-container class="fill-height">
+            <v-layout class="justify-center align-center">
+                <v-flex xs12 sm10 md9 lg8 xl7 class="grey lighten-3 elevation-10" style=" border-radius: 20px">
+                    <div style="padding: 20px 30px;">
+                        <p class="headline grey--text text--darken-3" style="margin: 0">User registered successfully, in
+                            <b>{{ time }}</b> you will be redirected to the starting page</p>
+                    </div>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
@@ -24,9 +23,9 @@
         },
         methods: {
             pushAfter3Sec() {
-                setTimeout( ()=> this.time = 2, 1000)
-                setTimeout( ()=> this.time = 1, 2000);
-                setTimeout( ()=> this.time = 0, 3000);
+                setTimeout(() => this.time = 2, 1000)
+                setTimeout(() => this.time = 1, 2000);
+                setTimeout(() => this.time = 0, 3000);
                 setTimeout( ()=> this.$router.push({ path: 'login' }), 3500);
             }
         },
