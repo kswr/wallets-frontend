58d50734630006c4f08a5159570fee9e02fce5fa
kswr <ksawery.oleszynski@gmail.com>
2019-01-28 15:12:51 +0100

    removed buttons from front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,11 @@
                 <v-layout class="justify-center align-center">
                     <v-flex xs10 sm7 md5 lg4 xl3 class="lighten-3" style=" border-radius: 20px">
                         <div>
-                            <p class="display-1 white--text text--darken-3">WPS WALLETS</p>
-                            <p class="white--text">Simple app for real-life viewing, managing and controlling your interactive sales wallet</p>
-                            <v-btn large color="light-green darken-1" class="white--text" to="login">Sign in</v-btn>
-                            <p class="white--text">or</p>
-                            <v-btn large color="light-green darken-1" class="white--text" to="signup">Create an account</v-btn>
+                            <!--<p class="display-1 white&#45;&#45;text text&#45;&#45;darken-3">WPS WALLETS</p>-->
+                            <!--<p class="white&#45;&#45;text">Simple app for real-life viewing, managing and controlling your interactive sales wallet</p>-->
+                            <!--<v-btn large color="light-green darken-1" class="white&#45;&#45;text" to="login">Sign in</v-btn>-->
+                            <!--<p class="white&#45;&#45;text">or</p>-->
+                            <!--<v-btn large color="light-green darken-1" class="white&#45;&#45;text" to="signup">Create an account</v-btn>-->
                         </div>
                     </v-flex>
                 </v-layout>
